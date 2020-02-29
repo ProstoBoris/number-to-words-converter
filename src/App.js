7d07235ab8449db0497d3a7ab4cafbf299b61151
Boris <developer.and.all@gmail.com>
2020-02-29 16:25:34 -0500

    Create ResultPage presentational component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import InputPage from './containers/InputPage';
+import ResultPage from './components/ResultPage';
 
 function App() {
   return (
     <div className="App">
       <h2>Number to Words Converter</h2>
       <InputPage />
+      <ResultPage />
     </div>
   );
 }
---
+++ src/components/ResultPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const resultPage = () => {
+    return (
+        <div>
+            <h4>List of Words</h4>
+        </div>
+    );
+}
+
+export default resultPage;
