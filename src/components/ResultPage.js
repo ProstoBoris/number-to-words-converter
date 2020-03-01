073fd6624c6b693d9f1a7ee04b9da02d4edbe220
Boris <developer.and.all@gmail.com>
2020-03-01 16:42:41 -0500

    Create Word.js to display words and pass data to it

--- src/components/ResultPage.js
+++ src/components/ResultPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Word from './Word';
 
 const resultPage = (props) => {
     return (
         <div>
             <h4>List of Words</h4>
-            <div>{props.words}</div>
+            <div className="row">{
+                props.words.map((word, index) => {
+                    return <Word word={word} key={index} />
+                })
+            }</div>
         </div>
     );
 }
---
+++ src/components/Word.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const word = (props) => {
+    return (
+        <div className="col-sm-3">
+            {props.word}
+        </div>
+    );
+}
+
+export default word;
