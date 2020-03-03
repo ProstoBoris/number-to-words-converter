4fa63fe120fe2efcaa25e209056161abe4d28d69
Boris <developer.and.all@gmail.com>
2020-03-03 17:36:57 -0500

    Add phone keypad image to ResultPage

--- src/components/ResultPage.js
+++ src/components/ResultPage.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Word from './Word';
+import keypadImg from '../assets/keypad.png';
 
 const resultPage = (props) => {
     return (props.words ?
-        <div className="container">
-            <h4>List of Words</h4>
-            <div className="row">{
-                props.words.map((word, index) => {
-                    return <Word word={word} key={index} />
-                })
-            }</div>
+        <div>
+            {props.words.length ?
+                <div className="container">
+                    <h4>List of Words</h4>
+                    <div className="row">{
+                        props.words.map((word, index) => {
+                            return <Word word={word} key={index} />
+                        })
+                    }</div>
+                </div> : <img src={keypadImg} alt="phone keypad" />}
         </div> : null
     );
 }
