63a75121a11413941c8a6eb6ff13b1253f174bd8
Boris <developer.and.all@gmail.com>
2020-03-03 19:17:41 -0500

    Add loading spinner

--- src/App.js
+++ src/App.js
@@ -5,19 +5,24 @@ import ResultPage from './components/ResultPage';
 
 class App extends Component {
   state = {
-    words: []
+    words: [],
+    loading: false
   }
 
   onGetResult = (childData) => {
-    this.setState({ words: [...childData] });
+    this.setState({ ...this.state, words: [...childData] });
+  }
+
+  onLoadingUpdate = (loading) => {
+    this.setState({ ...this.state, loading });
   }
 
   render() {
     return (
       <div className="App">
         <h2>Number to Words Converter</h2>
-        <InputPage getResult={this.onGetResult} />
-        <ResultPage words={this.state.words} />
+        <InputPage getResult={this.onGetResult} updateLoading={this.onLoadingUpdate} />
+        <ResultPage words={this.state.words} loading={this.state.loading} />
       </div>
     );
   }
--- src/components/ResultPage.js
+++ src/components/ResultPage.js
@@ -3,17 +3,36 @@ import Word from './Word';
 import keypadImg from '../assets/keypad.png';
 
 const resultPage = (props) => {
+
+    let loading = (
+        <div className="spinner-border text-success" role="status">
+            <span className="sr-only">Loading...</span>
+        </div>
+    );
+
+    const getStyle = () => {
+        if (props.loading) {
+            return {
+                display: props.loading ? 'none' : 'block'
+            }
+        }
+    }
+
     return (props.words ?
         <div>
             {props.words.length ?
-                <div className="container">
+                <div className="container result-conrainer">
                     <h4>List of Words</h4>
-                    <div className="row">{
+                    <div className="row" style={getStyle()}>{
                         props.words.map((word, index) => {
                             return <Word word={word} key={index} />
                         })
                     }</div>
-                </div> : <img src={keypadImg} alt="phone keypad" />}
+                </div> : (<img className="img-fluid"
+                    src={keypadImg}
+                    style={getStyle()}
+                    alt="phone keypad" />)}
+            {props.loading ? loading : ''}
         </div> : null
     );
 }
